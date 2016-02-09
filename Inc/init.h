1c372c96faed0f859aa5a4a5b678caa7430e5240
Roger Dahl <rogerdahl@dahlsys.com>
2016-02-09 14:19:17 -0700

    initial work

--- Inc/init.h
+++ Inc/init.h
@@ -1,3 +1,12 @@
 #pragma once
 
 void InitAll(void);
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC_Init(void);
+static void MX_LCD_Init(void);
+static void MX_TS_Init(void);
+static void Timebase_Init(void);
+
+static void Error_Handler(void);
--- Src/init.c
+++ Src/init.c
@@ -5,15 +5,6 @@
 #include "tsl.h"
 #include "tsl_user.h"
 
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_ADC_Init(void);
-static void MX_LCD_Init(void);
-static void MX_TS_Init(void);
-static void Timebase_Init(void);
-
-static void Error_Handler(void);
-
 // Also referenced in stm32l1xx_it.c
 TIM_HandleTypeDef timHandle;
 
